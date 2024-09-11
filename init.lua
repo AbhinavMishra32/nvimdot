eeaa87badb0b3ddfa17a1e55c22ce40d1c59641e
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-11 22:06:31 +0530

    update

--- init.lua
+++ init.lua
@@ -83,6 +83,8 @@ vim.keymap.set('i', 'jk', '<Esc>l')
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('n', '<C-a>', 'ggVG')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
